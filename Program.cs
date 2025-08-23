7642f344c79d8566f89a51f9db6c00e8c1cd232d
KenzoWVY <kenzowendell@gmail.com>
2025-08-23 20:16:30 -0300

    Add GET, PUT and DELETE for vehicles with validation

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using MinimalApi.Domain.ModelViews;
 using MinimalApi.Domain.Entities;
 
 #region Builder
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdminService, AdminService>();
@@ -25,25 +26,52 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 
 var app = builder.Build();
+
 #endregion
 
 #region Home
+
 app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
+
 #endregion
 
 #region Admins
+
 app.MapPost("/admins/login", ([FromBody] LoginDTO loginDTO, IAdminService adminService) =>
 {
     if (adminService.Login(loginDTO) != null)
         return Results.Ok("Sucessful login");
     else
         return Results.Unauthorized();
-}).WithTags("Admin");
+}).WithTags("Admins");
+
 #endregion
 
 #region Vehicles
+
+ValidationError validateDTO(VehicleDTO vehicleDTO)
+{
+    var validation = new ValidationError
+    {
+        Messages = new List<string>()
+    };
+
+    if (string.IsNullOrEmpty(vehicleDTO.Name))
+        validation.Messages.Add("Name cannot be empty");
+    if (string.IsNullOrEmpty(vehicleDTO.CarBrand))
+        validation.Messages.Add("Brand cannot be empty");
+    if (vehicleDTO.Year < 1950)
+        validation.Messages.Add("Only accepted years after 1950");
+
+    return validation;
+}
+
 app.MapPost("/vehicles", ([FromBody] VehicleDTO vehicleDTO, IVehicleService vehicleService) =>
 {
+    var validation = validateDTO(vehicleDTO);
+    if (validation.Messages.Count() > 0)
+        return Results.BadRequest(validation);
+
     var vehicle = new Vehicle
     {
         Name = vehicleDTO.Name,
@@ -53,19 +81,57 @@ app.MapPost("/vehicles", ([FromBody] VehicleDTO vehicleDTO, IVehicleService vehi
     vehicleService.Include(vehicle);
 
     return Results.Created($"/vehicle/{vehicle.Id}", vehicle);
-}).WithTags("Vehicle");
+}).WithTags("Vehicles");
 
 app.MapGet("/vehicles", ([FromQuery] int? page, IVehicleService vehicleService) =>
 {
     var vehicles = vehicleService.All(page);
 
     return Results.Ok(vehicles);
-}).WithTags("Vehicle");
+}).WithTags("Vehicles");
+
+app.MapGet("/vehicles/{id}", ([FromRoute] int id, IVehicleService vehicleService) =>
+{
+    var vehicle = vehicleService.SearchId(id);
+    if (vehicle == null) return Results.NotFound();
+
+    return Results.Ok(vehicle);
+}).WithTags("Vehicles");
+
+app.MapPut("/vehicles/{id}", ([FromRoute] int id, VehicleDTO vehicleDTO, IVehicleService vehicleService) =>
+{
+    var validation = validateDTO(vehicleDTO);
+    if (validation.Messages.Count() > 0)
+        return Results.BadRequest(validation);
+
+    var vehicle = vehicleService.SearchId(id);
+    if (vehicle == null) return Results.NotFound();
+
+    vehicle.Name = vehicleDTO.Name;
+    vehicle.CarBrand = vehicleDTO.CarBrand;
+    vehicle.Year = vehicleDTO.Year;
+    vehicleService.Update(vehicle);
+
+    return Results.Ok();
+}).WithTags("Vehicles");
+
+app.MapDelete("/vehicles/{id}", ([FromRoute] int id, IVehicleService vehicleService) =>
+{
+    var vehicle = vehicleService.SearchId(id);
+    if (vehicle == null) return Results.NotFound();
+
+    vehicleService.Remove(vehicle);
+
+    return Results.NoContent();
+}).WithTags("Vehicles");
+
 #endregion
 
 #region App
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+
 #endregion
